87b4abc8537c79ecca6a65c00ece65e13bf9a194
mazer <mazer@banquemisr.com>
2021-09-05 10:24:44 +0200

    tabbed view controllers

---
+++ EventsApp/TabbedModule/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Mena Gamal on 05/09/2021.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    @IBOutlet weak var segmentControll: UISegmentedControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindUI()
+    }
+    
+    
+    private func bindUI() {
+        segmentControll.rx.selectedSegmentIndex.subscribe(onNext: { index in
+            // replace View controllers
+            
+        }).disposed(by: disposeBag)
+    }
+}
+
