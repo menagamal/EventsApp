809061313f02fcbedd0d877be3edcf4e1b0e8020
mazer <mazer@banquemisr.com>
2021-09-05 09:57:07 +0200

    Initial Commit

---
+++ EventsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventsApp
+//
+//  Created by Mena Gamal on 05/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
